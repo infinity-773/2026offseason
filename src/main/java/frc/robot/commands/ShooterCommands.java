5935118140143ad30c25a2e97df805a889ca28bd
bing_de <3239480660@qq.com>
2026-07-06 20:16:06 +0800

    添加瞄准命令

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -4,20 +4,26 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter.Shooter;
 import frc.robot.subsystems.Shooter.ShooterConstants;
+import frc.robot.subsystems.drive.Drive;
 
 public class ShooterCommands extends Command {
   private final Shooter shooter;
-  private final Translation2d translation2d;
+  private final Drive drive;
+  private Translation2d translation2d;
 
-  public ShooterCommands(Shooter shooter, Translation2d translation2d) {
+  public ShooterCommands(Shooter shooter, Drive drive) {
     this.shooter = shooter;
-    this.translation2d = translation2d;
+    this.drive = drive;
     addRequirements(shooter);
   }
 
   @Override
-  public void initialize() {
-    double vel = Math.sqrt(translation2d.getDistance(ShooterConstants.GOAL)*(ShooterConstants.G/Math.sin(2 * ShooterConstants.SHOOTER_POS)));
+  public void execute() {
+    translation2d = drive.getPose().getTranslation();
+    double vel =
+        Math.sqrt(
+            translation2d.getDistance(ShooterConstants.GOAL)
+                * (ShooterConstants.G / Math.sin(2 * ShooterConstants.SHOOTER_POS)));
     shooter.shoot(ShooterConstants.SHOOTER_POS, ShooterConstants.FEEDER_1VOL, vel);
   }
 
