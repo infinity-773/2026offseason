76af202ab9800204674c0f2d878d5c4c0e7f3ac8
infinity-773 <15513055965@153.com>
2026-07-07 22:39:21 +0800

    暂存

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,6 +18,8 @@ public final class Constants {
   public static final Mode simMode = Mode.SIM;
   public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 
+  public static final boolean tuningMode = false;
+
   public static enum Mode {
     /** Running on a real robot. */
     REAL,
